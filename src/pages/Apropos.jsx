aca102c1f5183fd20e28b5ea6da8be9196413a34
Melodie <melodie.migault@gmail.com>
2025-05-09 18:24:25 +0200

    vite config @, pages and components

---
+++ src/pages/Apropos.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '@layout/Header';
+import Footer from '@layout/Footer';
+
+const Apropos = () => {
+    return (
+        <>
+            <Header/>
+            <div>
+                A propos
+            </div>
+            <Footer/>
+        </>
+        
+    );
+};
+
+export default Apropos;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+      {
+        find: '@layout',
+        replacement: path.resolve(__dirname, 'src/components/layout')
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages')
+      }
+    ]
+  }
 })
