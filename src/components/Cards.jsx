634aa3681e0b0a89161c10f8f6ab02d42f36e3a1
Melodie <melodie.migault@gmail.com>
2025-06-10 18:52:14 +0200

    CSS App & cards on going

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const Cards = () => {
-    const [gallery, setGallery] = useState({})
-    useEffect(() => {
-        fetch(`http://localhost:8080/api/properties`)
-            .then((response) => response.json()
-                .then((data) => setGallery(data))
-                .catch((error) => console.log(error))
-            )        
-    },[])
+import '@assets/style_components/cards.css'
 
-
+const Cards = ({gal}) => {
     return (
-        <div>
-            {gallery.map(img => (
-                <div key={img.id}>
-                    <img src={img.cover} alt={img.title}/>
-                    <h2>{img.title}</h2>
-                </div>
-            ))}
-        </div>
+        <article className='galleryArticle' id={gal.id}>
+            <img src={gal.cover} alt={gal.title}/>
+            <h2>{gal.title}</h2>
+        </article>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 
 import Banner from '@components/Banner.jsx'
 import Cards from '@components/Cards.jsx'
 
 const Home = () => {
+    const [gallery, setGallery] = useState()
+    const flag = useRef(false)
+    const [isLoad, setLoad] = useState(false)
+
+    useEffect(() => {
+        if(flag.current === false){
+            fetch(`http://localhost:8080/api/properties`)
+                .then((response) => response.json()
+                    .then((data) => {
+                        setGallery(data)
+                        setLoad(true)
+                    })
+                    .catch((error) => console.log(error))
+                )   
+            }
+        
+        return () => flag.current = true
+    },[])
+
+    if(!isLoad){
+        return <div>Chargement...</div>
+    }
+
     return (
-        <>               
-            <main className="home">
-                <Banner
-                    title="Chez vous, partout et ailleurs"
-                    imageSRC="/images/Banner1.png"
-                />
-                
-                Page d'accueil
-                <Cards/>
-            </main>
-        </>
-    );
-};
+        <div className="home">
+            <Banner
+                title="Chez vous, partout et ailleurs"
+                imageSRC="/images/Banner1.png"
+            />
+
+            <section className='homeGallery'>
+                {
+                    gallery.map(gal => (
+                        <Cards key={gal.id} gal={gal}/>
+                    ))
+                }
+            </section>    
+            
+               
+        </div>
+    )
+}
 
 export default Home;
