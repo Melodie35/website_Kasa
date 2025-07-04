7e46bd0acbbdb7b23117aec90484b45122297fd7
Melodie <melodie.migault@gmail.com>
2025-07-04 11:38:54 +0200

    config test jest, babel + change in package.json + banner.test.js ok

---
+++ src/test/banner.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "@testing-library/react"
+
+
+import Banner from '@components/Banner.jsx'
+
+describe('Banner',() => {
+    test('Should render without crash', () => {
+        render(<Banner/>);
+    })
+})
